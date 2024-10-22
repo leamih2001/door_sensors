f7cfebc2eab702d5dff9ead67c7adb0db73073aa
leamih2001 <134271702+leamih2001@users.noreply.github.com>
2024-10-22 21:57:56 +0200

    Update detectionService.py

--- detectionService.py
+++ detectionService.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, jsonify
 import cv2
+import os
+
+image_folder = "captured_images"
+if not os.path.exists(image_folder):
+    os.makedirs(image_folder)
 
 app = Flask(__name__)
 
@@ -16,15 +21,22 @@ def detect_faces():
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
-    cv2.imshow("Gray Image", gray)
-    cv2.waitKey(0)
-    
     if face_cascade.empty():
         return jsonify({"status": "error", "message": "Failed to load Haar Cascade XML file"}), 500
 
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(20, 20))
 
+    for (x, y, w, h) in faces:
+        start_point = (x, y)
+        end_point = (x+w, y+h)
+        color = (255, 0, 0)
+        thickness = 2
 
+        image = cv2.rectangle(image, start_point, end_point, color, thickness)
+
+    image_path = image_path[:-4] + "_detectedFaces.jpg"
+    cv2.imwrite(image_path, image)
+    
     face_count = len(faces)
     faces_coords = [{"x": int(x), "y": int(y), "w": int(w), "h": int(h)} for (x, y, w, h) in faces]
 
