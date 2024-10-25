6eb2aafeee6bf20e22d723b78210e9dc2167f38c
leamih2001 <134271702+leamih2001@users.noreply.github.com>
2024-10-25 11:17:30 +0200

    Update captureService.py

--- captureService.py
+++ captureService.py
@@ -9,7 +9,7 @@ image_folder = "captured_images"
 if not os.path.exists(image_folder):
     os.makedirs(image_folder)
 
-cam = cv2.VideoCapture(0) 
+cam = cv2.VideoCapture(2, cv2.CAP_V4L2) 
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
@@ -22,6 +22,10 @@ ret, frame = cam.read()
 
 @app.route('/capture', methods=['POST'])
 def capture_image():
+
+    for _ in range (5):
+        cam.grab()
+
     ret,frame =cam.read()
 
     if not ret:
@@ -30,6 +34,10 @@ def capture_image():
    
     now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     image_path = os.path.join(image_folder, f"snapshot_{now}.jpg")
+    #frame = cv2.rotate(frame, cv2.ROTATE_180)
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
     cv2.imwrite(image_path, frame)
 
     return jsonify({"status": "success", "image_path": image_path}), 200
